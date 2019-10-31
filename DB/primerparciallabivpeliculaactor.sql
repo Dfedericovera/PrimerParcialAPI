230639a19c5b62e0ad66a52be3e87146c14d5a38
Fede <dfedericovera@gmail.com>
2019-10-31 17:54:01 -0300

    database

---
+++ DB/primerparciallabivpeliculaactor.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 31-10-2019 a las 21:53:11
+-- Versión del servidor: 10.1.35-MariaDB
+-- Versión de PHP: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `primerparciallabivpeliculaactor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actores`
+--
+
+CREATE TABLE `actores` (
+  `id` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(66) NOT NULL,
+  `Nacionalidad` varchar(66) NOT NULL,
+  `FechaDeNacimiento` varchar(66) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `actores`
+--
+
+INSERT INTO `actores` (`id`, `Nombre`, `Apellido`, `Nacionalidad`, `FechaDeNacimiento`) VALUES
+(1, 'Silvester', 'Stallone', 'EEUU', '2019-10-05'),
+(4, 'johnny ', 'depp', 'EEUU', '2019-10-11');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `id` int(5) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Tipo` varchar(50) NOT NULL,
+  `FechaDeEstreno` varchar(50) NOT NULL,
+  `CantidadDePublico` int(50) NOT NULL,
+  `Foto` varchar(50) NOT NULL,
+  `EstrellaPrincipal` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `peliculas`
+--
+
+INSERT INTO `peliculas` (`id`, `Nombre`, `Tipo`, `FechaDeEstreno`, `CantidadDePublico`, `Foto`, `EstrellaPrincipal`) VALUES
+(1, 'X-men', 'Accion', '24/09/2000', 25500000, '', ''),
+(3, 'Harry Potter', 'Otros', '2019-10-29', 2134324234, '', '');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `actores`
+--
+ALTER TABLE `actores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `actores`
+--
+ALTER TABLE `actores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
